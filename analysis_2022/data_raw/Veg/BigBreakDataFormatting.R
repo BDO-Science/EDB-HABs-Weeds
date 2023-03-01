f2b187b1cba8a9bd5714151e5134ae5e0af63cd1
github_username <nbertrand@usbr.gov>
2023-03-01 15:47:40 -0800

    I am attempting to due a final push

--- analysis_2022/data_raw/Veg/BigBreakDataFormatting.R
+++ analysis_2022/data_raw/Veg/BigBreakDataFormatting.R
@@ -1,84 +1,7 @@
-#Final Code used in 2022 submerged aquatic vegetion in HABS report. This is modified from code written by DWR staff as noted below.
-#The original code contained some exploratory efforts not needed to generate the figures for the report.
-#This code is streamlined to convert 2022 datafiles (which were formatted differently from 2021) and
-#only generate the figueres needed for the report.
+#final clean up of this code was consolidate in the file CSTARS DataReformatting BB SJ FT.R
+#use that file as starting point.
 
 
-
-#2022 author Nick Bertrand
-#Nbertrand@usbr.gov
-#Edited in 2022 by Nick Bertrand nbertrand@usbr.gov
-
-
-# Aquatic Vegetation Project Work Team
-# Master data set
-# Submersed aquatic vegetation
-
-#original code author
-#Nick Rasmussen
-#nicholas.rasmussen@water.ca.gov
-
-#####################
-########
-#####
-#2022 CSTARS data reformatting to fit the CSTARS_2021_GroundTruthing_SAV.R structure
-#franks Tract data
-
-library(tidyverse)
-library(splitstackshape)
-library(sf)
-
-cstars22.start <- read_csv("https://raw.githubusercontent.com/BDO-Science/EDB-HABs-Weeds/main/analysis_2022/data_raw/Veg/FranksTract_2022_formatted.csv")
-
-view(cstars22.start)
-
-#gather function consolidates the species into a count column and drops out any NAs from the data.
-cstars22.gath <- cstars22.start %>% gather("species","rake_prop", Richardson:Unknown) %>%
-  drop_na(rake_prop)
-#view(cstars22.gath)
-
-#gather function consolidates the species into a count column and drops out any NAs from the data.
-cstars22.gath <- cstars22.start %>% gather("species","rake_prop", Richardson:Unknown) %>%
-  drop_na(rake_prop)
-#view(cstars22.gath)
-
-
-library(sf)
-
-wgs84conversion <- cstars22.gath %>%
-  st_as_sf(coords = c("easting_26910", "northing_26910"), crs = 26910) %>%
-  st_transform(4326) %>%
-  st_coordinates()
-
-wgs84conversion <-as.data.frame(wgs84conversion)
-
-#view(wgs84conversion)
-
-wgs84conversion<- rename(wgs84conversion, latitude_wgs84 = Y , longitude_wgs84 = X)
-
-cstars22.con <-cbind(cstars22.gath, wgs84conversion) %>%
-  select(latitude_wgs84, longitude_wgs84, date, time, feat,rake_teeth, species, rake_prop) %>%
-  rename(survey_method = feat, rake_teeth_corr = rake_teeth)
-
-
-cstars22.con$time <- as.character(cstars22.con$time)
-#view(cstars22.con)
-
-#####################################
-##############
-#########
-
-#data reformatting for the Big Break, San Joaquin sites begins here and colminates in combining Franks tract, Big break,
-#and San Joaquin into a single data frame to be used by the CSTARS ground Truthing file.
-
-#original file taken from link below.
-#https://github.com/InteragencyEcologicalProgram/AquaticVegetationPWT/blob/e8efc9630dbdcddaa68d2255f9ee3d39f8e88777/MasterDataSet_SAV/Scripts_DataFormatting/CSTARS_GroundTruthing_DataFormatting_FranksTract2022.R
-
-#Edited in 2022 by Nick Bertrand nbertrand@usbr.gov
-#edited to reformat BBgrid_2022.dbf
-##############################################
-#############################
-#############
 
 #install.packages('janitor', dependencies=TRUE, repos='http://cran.rstudio.com/')
 
@@ -110,8 +33,10 @@ library(plotrix) #standard error function
 
 # Read in the data----------------------------------------------
 
-#2022 Big Break data
+#2022 Big Break data collected by CSTARS in July
 bb <- read.dbf("C:/Users/nbertrand/Desktop/Bertrand/GitHub/EDB-HABs-Weeds/analysis_2022/data_raw/Veg/BBgrid_2022.dbf")
+
+#class(bb$GPS_Time)
 
 bb$GPS_Time <- as.character(parse_date_time(bb$GPS_Time, "%I:%M:%S%p"))
 bb$GPS_Time <- as.POSIXct(bb$GPS_Time)
@@ -213,7 +138,45 @@ bb_long <- bb22 %>%
                , values_to = "value")  %>%
   glimpse()
 
+#view(ft_long)
 
+# #create subset of data that is not SAV
+# ft_nonsav <- bb22 %>%
+#   #use janitor function to clean up column names
+#   clean_names() %>%
+#   #rename some columns
+#   rename(
+#     ucd_id = point_id
+#     #,sepro_id = grid_id
+#     #CSTARS said the distances are in meters
+#     #,pt_dist_m = near_dist
+#     ,date = gps_date
+#     ,time = gps_time
+#     ,feat = feat_name
+#     ,feat2 = species_1
+#     #CRS is UTM NAD 83 (Zone 10N) (EPSG = 26910)
+#     ,northing_26910 = northing
+#     ,easting_26910 = easting) %>%
+#   #only keep non-SAV
+#   filter(feat!="SAV") %>%
+#   #only keep needed columns
+#   select(ucd_id
+#          #,sepro_id
+#          #,pt_dist_m
+#          ,northing_26910
+#          ,easting_26910
+#          ,date
+#          ,time
+#          ,feat
+#          ,feat2
+#   ) %>%
+#   #remove duplicate rows
+#   distinct() %>%
+#   #drop feat and replace with feat2
+#   select(-feat) %>%
+#   rename(feat=feat2)
+#
+# #view(ft_nonsav)
 #convert from long to wideish
 bb_wide <- bb_long %>%
   #now pivot back to a bit wider
@@ -247,6 +210,116 @@ bb_wider <- bb_wide %>%
   pivot_wider(names_from = "species", values_from = "rake_prop")
 #view(bb_wider)
 
+# #add non-SAV back into main data set
+# ft_all_clean <- bind_rows(ft_wider,ft_nonsav) %>%
+#   arrange(sepro_id)
+
+#write final sepro formatted data frame to csv file
+#write_csv(ft_all_clean,file = "Data_Raw/CSTARS_GroundTruthing/FranksTract_2022/FranksTract_2022_formatted.csv")
+
+# #format final long form data
+# #NOTE: this data set doesn't include the three non-SAV samples
+# ft_longer <- ft_wider %>%
+#   pivot_longer(cols=Richardson:Unknown #select range of columns
+#                , names_to = "species" #specify column name
+#                , values_to = "perc") %>% #specify column name
+#   mutate(
+#     #species present in trace amounts are indicated with perc = 0; change this from 0 to 1
+#     #then change all NAs for perc to 0, indicating species absence
+#     perc_trace = case_when(perc==0 ~ 1,is.na(perc)~0,TRUE ~ perc)
+#     #add column that calculates absolute rake coverage by spp within sample
+#     ,rake_index = (rake_teeth/100)*(perc_trace/100)) %>%
+#   glimpse()
+
+# #create summary data frame
+# ft_longer_sum <- ft_longer %>%
+#   #calculate summary stats by species
+#   group_by(species) %>%
+#   summarize(
+#     rake_mean = mean(rake_index)
+#     ,rake_se = std.error(rake_index)
+#     #,rake_n = count()
+#     , .groups = 'drop'
+#   )
+
+# #Make map of the points-------------
+#
+# #look at WW_Delta base map CRS
+# #st_crs(WW_Delta)
+# #CRS = NAD83, which is different than our sample data points
+# #EPSG: 4269
+#
+# #add geometry to UCD coordinates
+# ft_all_geo <- ft_all_clean %>%
+#   #specify the CRS of original coordinate
+#   #confirmed that it is UTM NAD 83 (Zone 10N) (EPSG = 26910)
+#   st_as_sf(coords = c("easting_26910", "northing_26910"), crs = 26910) %>%
+#   #then transform to NAD83, which is CRS of base layer
+#   st_transform(4269) %>%
+#   glimpse()
+#
+# #2017-2020 GPX file: transform coordinates to base layer CRS
+# seprog <- sepro %>%
+#   st_transform(crs = 4269) #transform to NAD83
+#
+# #create map comparing CSTARS and Sepro survey grids
+# (map_both_grids <- ggplot()+
+#     #plot waterways base layer
+#     geom_sf(data= WW_Delta, fill= "lightsteelblue1", color= "black") +
+#     #plot the Sepro grid (n=100)
+#     geom_sf(data= seprog, fill= "black", color= "black", shape= 23, size= 4) +
+#     #plot the CSTARS grid, which is most of the odd numbered sepro points (n = 45)
+#     geom_sf(data= ft_all_geo, aes(fill= feat), color= "black", shape= 21, size= 3) +
+#     scale_fill_manual(
+#       name = "CSTARS Veg Type",
+#       labels=c('SAV','Tule','Water Primrose'),
+#       values=c("deepskyblue","darkolivegreen1","gold")
+#     )+
+#     #zoom in on region of delta where samples were collected
+#     #just eyeballed the range from google maps
+#     coord_sf(
+#       xlim =c(-121.56, -121.64),
+#       ylim = c(38.07, 38.02)
+#     )+
+#     theme_bw()+
+#     ggtitle('CSTARS Survey: July 29, 2022')
+# )
+# #ggsave(file = "Data_Raw/CSTARS_GroundTruthing/FranksTract_2022/FranksTract_2022_CSTARS_SePRO.png",type ="cairo-png",width=10, height=7,units="in",dpi=300)
+#
+# #create map showing CSTARS rake sample size
+# (map_sav_size <- ggplot()+
+#     #plot waterways base layer
+#     geom_sf(data= WW_Delta, fill= "lightsteelblue1", color= "black") +
+#     #plot the CSTARS SAV with point size representing sample size
+#     geom_sf(data= ft_all_geo, fill= "deepskyblue", color= "black", shape= 21, aes(size= rake_teeth)) +
+#     #zoom in on region of delta where samples were collected
+#     #just eyeballed the range from google maps
+#     coord_sf(
+#       xlim =c(-121.56, -121.64),
+#       ylim = c(38.07, 38.02)
+#     )+
+#     theme_bw()+
+#     ggtitle('CSTARS Survey: July 29, 2022')
+# )
+# #ggsave(file = "Data_Raw/CSTARS_GroundTruthing/FranksTract_2022/FranksTract_2022_CSTARS_RakeSampleSize.png",type ="cairo-png",width=10, height=7,units="in",dpi=300)
+#
+# #Summarize the community composition----------
+#
+# #make histogram of total rake cover
+#
+#
+# #plot species mean abundances ordered by mean prop of rake covered
+# (plot_spp_score_avg <-ggplot(ft_longer_sum, aes(x=fct_reorder(species,-rake_mean), y= rake_mean))+
+#    geom_bar(stat = "identity") +
+#    geom_errorbar(aes(ymin=rake_mean-rake_se, ymax=rake_mean+rake_se), width = 0.2) +
+#    ylab("Mean proportion of rake head covered") + xlab("Species")
+# )
+# #ggsave(file = "Data_Raw/CSTARS_GroundTruthing/FranksTract_2022/FranksTract_2022_CSTARS_SppAbundance.png",type ="cairo-png",width=8, height=5,units="in",dpi=300)
+#
+#
+#
+#
+
 ########################
 ##############
 ########
@@ -271,12 +344,12 @@ wgs84conversion <-as.data.frame(wgs84conversion)
 
 wgs84conversion<- rename(wgs84conversion, latitude_wgs84 = Y , longitude_wgs84 = X)
 
-#creates big break dataframe to be joined to final data frame
 bb22.con <-cbind(bb22.gath, wgs84conversion) %>%
   select(latitude_wgs84, longitude_wgs84, date, time,feat,rake_teeth, species, rake_prop) %>%
   rename(survey_method = feat, rake_teeth_corr = rake_teeth)
 
-#view(bb22.con)
+view(bb22.con)
+
 
 #######################
 ###############
@@ -284,6 +357,8 @@ bb22.con <-cbind(bb22.gath, wgs84conversion) %>%
 
 #code is the same as above but modified to accept the SAVFLOAT.dbf data file which is the data from the
 #SanJoaquin River sampling
+#this code is much more aggressively edited than the above code to elimate unneccary clutter
+
 
 # Read in the data----------------------------------------------
 
@@ -423,6 +498,8 @@ sj_wider <- sj_wide %>%
   pivot_wider(names_from = "species", values_from = "rake_prop")
 #view(sj_wider)
 
+
+
 ########################
 ##############
 ########
@@ -447,7 +524,6 @@ wgs84conversion <-as.data.frame(wgs84conversion)
 
 wgs84conversion<- rename(wgs84conversion, latitude_wgs84 = Y , longitude_wgs84 = X)
 
-#this generates data frame to be joined to the final dataframe
 sj22.con <-cbind(sj22.gath, wgs84conversion) %>%
   select(latitude_wgs84, longitude_wgs84, date, time,feat,rake_teeth, species, rake_prop) %>%
   rename(survey_method = feat, rake_teeth_corr = rake_teeth)
@@ -455,10 +531,12 @@ sj22.con <-cbind(sj22.gath, wgs84conversion) %>%
 #view(sj22.con)
 
 ##############################
+
 ########
 ###
-
 #begin combining reforamtted data.
+
+
 bbsj <- union(bb22.con, sj22.con)
 
 #view(bbsj)
@@ -469,4 +547,3 @@ cstarsbbsj <- union(cstars22.con,bbsj)
 
 cstars <- cstarsbbsj
 view(cstars)
-

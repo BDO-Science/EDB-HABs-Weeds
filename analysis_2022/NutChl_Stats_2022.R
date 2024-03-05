10d39a28905aa62dcc4ef24fa75c5fd776e66b3f
Cat Pien <catarina.pien@gmail.com>
2024-03-04 16:33:14 -0800

    adding analyses that were not pushed

--- analysis_2022/NutChl_Stats_2022.R
+++ analysis_2022/NutChl_Stats_2022.R
@@ -57,18 +57,51 @@ sampsize <- nuts_stats_sf %>%
 
 ggplot(sampsize) + geom_tile(aes(x = fYear, y = Season, fill = n))
 
+data_year <- nuts_stats_sf %>%
+  dplyr::filter(Stratum2 %in% c("Lower Sac", "Lower SJ","OMR", "South Delta", "Franks"))
+  # dplyr::filter(DissAmmonia_Sign=="=",
+  #               DissNitrateNitrite_Sign=="=",
+  #               DissOrthophos_Sign=="=")
+
+
+# Datasets for each analyte - removed all the below reporting limit instances
+data_nit <- data_year %>% filter(DissNitrateNitrite_Sign == "=") %>%
+  filter(!is.na(DissNitrateNitrite))
+data_amm <- data_year %>% filter(DissAmmonia_Sign == "=") %>%
+  filter(!is.na(DissAmmonia))
+data_chl <- data_year %>% filter(Chlorophyll_Sign == "=") %>%
+  filter(!is.na(Chlorophyll))
+data_ortho <- data_year %>% filter(DissOrthophos_Sign == "=") %>%
+  filter(!is.na(DissOrthophos))
+
+# Plots for each analyte
+ggplot(data_nit) + geom_point(aes(x = Date, y = DissNitrateNitrite)) + facet_wrap(~Stratum2)
+ggplot(data_amm) + geom_point(aes(x = Date, y = Ammonium)) + facet_wrap(~Stratum2)
+ggplot(data_ortho) + geom_point(aes(x = Date, y = DissOrthophos)) + facet_wrap(~Stratum2)
+ggplot(data_chl) + geom_point(aes(x = Date, y = Chlorophyll)) + facet_wrap(~Stratum2)
+
+# Look at sample size
+sampsize <- data_year %>%
+  dplyr::group_by(fMonth, fYear) %>%
+  dplyr::summarize(n = n())
+
+ggplot(sampsize) + geom_tile(aes(x = fYear, y = fMonth, fill = n)) +
+  geom_text(aes(label = n, y = fMonth, x = fYear), color = "white")
+
 # Nitrate ----------------------------------------------------------
 #First let's do nitrate. Heres's where the reporting limit thing could mess us up.
-nit = lmer(log(Nitrate+0.04) ~ fYear  + Stratum2 + (1|fMonth) ,  data = nuts_stats_sf)
+#nit = lmer(log(Nitrate+0.04) ~ fYear  + Stratum2 + (1|fMonth) ,  data = nuts_stats_sf)
+nit = lmer(log(Nitrate+1) ~ fYear  + (1|fMonth) ,  data = data_nit)
 summary(nit)
 plot(nit)
+
 
 # Residuals
 nitres = simulateResiduals(nit)
 plot(nitres)
 par(mfrow = c(2,4))
 resid1 = resid(nit, type = "pearson")
-df1 = nuts_stats_sf %>% filter(!is.na(Nitrate), !is.na(fMonth), !is.na(Stratum2), !is.na(fYear))
+df1 = data_year %>% dplyr::filter(!is.na(Nitrate), !is.na(fMonth), !is.na(fYear))
 lev1a = hatvalues(nit)
 plot(nit)
 qqnorm(resid1)
@@ -83,7 +116,8 @@ acf(resid1)
 
 
 #Ammonium--------------------------------------------------------------
-Amm = lmer(log(Ammonium+0.05) ~ fYear + Stratum2 + (1|fMonth),  data = nuts_stats_sf)
+#Amm = lmer(log(Ammonium+1) ~ fYear + Stratum2 +  (1|fMonth),  data = nuts_stats_sf)
+Amm = lmer(log(Ammonium+1) ~ fYear +  (1|fMonth),  data = data_amm)
 summary(Amm)
 plot(simulateResiduals(Amm))
 plot(Amm)
@@ -91,7 +125,7 @@ plot(Amm)
 # Residuals
 par(mfrow = c(2,4))
 residamm = resid(Amm, type = "pearson")
-df_amm = nuts_stats_sf %>% filter(!is.na(Ammonium), !is.na(fMonth), !is.na(Stratum2), !is.na(fYear))
+df_amm = data_year %>% filter(!is.na(Ammonium), !is.na(fMonth), !is.na(Stratum2), !is.na(fYear))
 lev1a = hatvalues(Amm)
 plot(Amm)
 qqnorm(residamm)
@@ -106,7 +140,8 @@ acf(residamm)
 
 
 #Chlorophyll -------------------------------------------------------------
-chl= lmer(log(Chl+1) ~ fYear + Stratum2 + (1|fMonth),  data = nuts_stats_sf)
+#chl= lmer(log(Chl+1) ~ fYear  + Stratum2 + (1|fMonth),  data = nuts_stats_sf)
+chl= lmer(log(Chl+1) ~ fYear  + (1|fMonth),  data = data_chl)
 summary(chl)
 plot(simulateResiduals(chl))
 plot(chl)
@@ -115,7 +150,7 @@ plot(chl)
 # Residuals
 par(mfrow = c(2,4))
 residchl = resid(chl, type = "pearson")
-df_chl = nuts_stats_sf %>% filter(!is.na(Chl), !is.na(fMonth), !is.na(Stratum2), !is.na(fYear))
+df_chl = data_chl %>% filter(!is.na(Chl), !is.na(fMonth), !is.na(Stratum2), !is.na(fYear))
 lev1a = hatvalues(chl)
 plot(chl)
 qqnorm(residchl)
@@ -129,7 +164,8 @@ acf(residchl)
 
 
 #orthophosphate ------------------------------------------------------------
-Orth= lmer(log(Phosphorus+0.05) ~ fYear + Stratum2 + (1|fMonth),  data = nuts_stats_sf)
+#Orth= lmer(log(Phosphorus+0.05) ~ fYear + Stratum2 + (1|fMonth),  data = nuts_stats_sf)
+Orth= lmer(log(Phosphorus+1) ~ fYear  + (1|fMonth),  data = data_ortho)
 summary(Orth)
 plot(Orth)
 plot(simulateResiduals(Orth))
@@ -137,7 +173,7 @@ plot(simulateResiduals(Orth))
 # Residuals
 par(mfrow = c(2,4))
 residorth = resid(Orth, type = "pearson")
-df_Orth = nuts_stats_sf %>% filter(!is.na(Phosphorus), !is.na(fMonth), !is.na(Stratum2), !is.na(fYear))
+df_Orth = data_ortho %>% filter(!is.na(Phosphorus), !is.na(fMonth), !is.na(Stratum2), !is.na(fYear))
 lev1a = hatvalues(Orth)
 plot(Orth)
 qqnorm(residorth)
@@ -148,6 +184,80 @@ plot(residorth)
 plot(df_Orth$fYear, residorth)
 plot(df_Orth$Stratum2, residorth)
 acf(residorth)
+
+
+# CIs
+
+library(effects)
+library(emmeans)
+
+
+
+
+# emmeans tables
+# need to update dataset if we are going to use the updated datasets
+effects_amm <- effects::effect(term = "fYear", mod = Amm)
+summary(effects_amm)
+
+Amm.emm1 <- emmeans(Amm, "fYear", data=data_year)
+Ammresults1 <- as.data.frame(pairs(Amm.emm1, adjust="tukey")) %>%
+  mutate(sig = ifelse(p.value<0.05, "sig", "not sig")) %>%
+  arrange(sig)
+plot(Amm.emm1, comparisons = TRUE,type = "response")
+
+# Amm.emm2 <- emmeans(Amm, "Stratum2", data=data_year)
+# Ammresults2 <- as.data.frame(pairs(Amm.emm2, adjust="tukey")) %>%
+#   mutate(sig = ifelse(p.value<0.05, "sig", "not sig")) %>%
+#   arrange(sig)
+
+effects_nit <- effects::effect(term = "fYear", mod = nit)
+summary(effects_nit)
+
+Nit.emm1 <- emmeans(nit, "fYear", data=data_year)
+(Nitresults1 <- as.data.frame(pairs(Nit.emm1, adjust="tukey"))%>%
+    mutate(sig = ifelse(p.value<0.05, "sig", "not sig")) %>%
+    arrange(sig))
+plot(Nit.emm1, comparisons = TRUE,type = "response")
+
+# Nit.emm2 <- emmeans(nit, "Stratum2", data=data_year)
+# (Nitresults2 <- as.data.frame(pairs(Nit.emm2, adjust="tukey"))%>%
+#     mutate(sig = ifelse(p.value<0.05, "sig", "not sig")) %>%
+#     arrange(sig))
+effects_chl <- effects::effect(term = "fYear", mod = chl)
+summary(effects_chl)
+
+Chl.emm1 <- emmeans(chl, "fYear", data=data_year)
+(Chlresults1 <- as.data.frame(pairs(Chl.emm1, adjust="tukey"))%>%
+    mutate(sig = ifelse(p.value<0.05, "sig", "not sig")) %>%
+    arrange(sig))
+plot(Chl.emm1, comparisons = TRUE,type = "response")
+
+# Chl.emm2 <- emmeans(chl, "Stratum2", data=data_year)
+# (Chlresults2 <- as.data.frame(pairs(Chl.emm2, adjust="tukey"))%>%
+#     mutate(sig = ifelse(p.value<0.05, "sig", "not sig")) %>%
+#     arrange(sig))
+effects_orth <- effects::effect(term = "fYear", mod = Orth)
+summary(effects_orth)
+
+Orth.emm1 <- emmeans(Orth, "fYear", data=data_year)
+(Orthresults1 <- as.data.frame(pairs(Orth.emm1, adjust="tukey"))%>%
+    mutate(sig = ifelse(p.value<0.05, "sig", "not sig")) %>%
+    arrange(sig))
+plot(Orth.emm1, comparisons = TRUE,type = "response")
+
+# Orth.emm2 <- emmeans(Orth, "Stratum2", data=data_year)
+# (Orthresults2 <- as.data.frame(pairs(Orth.emm2, adjust="tukey"))%>%
+#     mutate(sig = ifelse(p.value<0.05, "sig", "not sig")) %>%
+#     arrange(sig))
+
+
+
+
+
+
+
+
+
 
 #plot for report##################
 #This is figure 2-26
@@ -171,47 +281,6 @@ Orthg = plot(emmeans(Orth, specs = "Year"), comparison = T)+
 
 cowplot::plot_grid(Amg, CHLg, Nitg, Orthg, nrow = 2)
 
-# emmeans tables
-
-Amm.emm1 <- emmeans(Amm, "fYear", data=nuts_stats_sf)
-Ammresults1 <- as.data.frame(pairs(Amm.emm1, adjust="tukey")) %>%
-  mutate(sig = ifelse(p.value<0.05, "sig", "not sig")) %>%
-  arrange(sig)
-
-Amm.emm2 <- emmeans(Amm, "Stratum2", data=nuts_stats_sf)
-Ammresults2 <- as.data.frame(pairs(Amm.emm2, adjust="tukey")) %>%
-  mutate(sig = ifelse(p.value<0.05, "sig", "not sig")) %>%
-  arrange(sig)
-
-Nit.emm1 <- emmeans(nit, "fYear", data=nuts_stats_sf)
-(Nitresults1 <- as.data.frame(pairs(Nit.emm1, adjust="tukey"))%>%
-    mutate(sig = ifelse(p.value<0.05, "sig", "not sig")) %>%
-    arrange(sig))
-
-Nit.emm2 <- emmeans(nit, "Stratum2", data=nuts_stats_sf)
-(Nitresults2 <- as.data.frame(pairs(Nit.emm2, adjust="tukey"))%>%
-    mutate(sig = ifelse(p.value<0.05, "sig", "not sig")) %>%
-    arrange(sig))
-
-Chl.emm1 <- emmeans(chl, "fYear", data=nuts_stats_sf)
-(Chlresults1 <- as.data.frame(pairs(Chl.emm1, adjust="tukey"))%>%
-    mutate(sig = ifelse(p.value<0.05, "sig", "not sig")) %>%
-    arrange(sig))
-
-Chl.emm2 <- emmeans(chl, "Stratum2", data=nuts_stats_sf)
-(Chlresults2 <- as.data.frame(pairs(Chl.emm2, adjust="tukey"))%>%
-    mutate(sig = ifelse(p.value<0.05, "sig", "not sig")) %>%
-    arrange(sig))
-
-Orth.emm1 <- emmeans(Orth, "fYear", data=nuts_stats_sf)
-(Orthresults1 <- as.data.frame(pairs(Orth.emm1, adjust="tukey"))%>%
-  mutate(sig = ifelse(p.value<0.05, "sig", "not sig")) %>%
-  arrange(sig))
-
-Orth.emm2 <- emmeans(Orth, "Stratum2", data=nuts_stats_sf)
-(Orthresults2 <- as.data.frame(pairs(Orth.emm2, adjust="tukey"))%>%
-    mutate(sig = ifelse(p.value<0.05, "sig", "not sig")) %>%
-    arrange(sig))
 
 
 # ggsave(filename = "Nutsemmeans.tiff", device = "tiff", path = "plots/",
